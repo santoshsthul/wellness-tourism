8fa3420596909c1ef47e186cbaf5373ffef130ac
santoshsthul <santoshsthul@gmail.com>
2026-02-01 11:29:28 +0530

    # Build relative workspace path

--- tourism_project/model_building/data_register.py
+++ tourism_project/model_building/data_register.py
@@ -1,6 +1,7 @@
 from huggingface_hub.utils import RepositoryNotFoundError, HfHubHTTPError
 from huggingface_hub import HfApi, create_repo
 import os
+from pathlib import Path
 
 repo_id = "SantoshS23/TourismPackage"
 repo_type = "dataset"
@@ -16,6 +17,10 @@ except RepositoryNotFoundError:
     print(f"Space '{repo_id}' not found. Creating new space...")
     create_repo(repo_id=repo_id, repo_type=repo_type, private=False)
     print(f"Space '{repo_id}' created.")
+
+# Build relative workspace path
+workspace = Path(os.getenv("GITHUB_WORKSPACE", "."))
+data_dir = workspace / "tourism_project" / "data"
 
 api.upload_folder(
     folder_path= os.path.join(tourism_project, 'data'),
